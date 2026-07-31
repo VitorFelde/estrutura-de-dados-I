19180837ba3bd31f4282761a19631cb781e0263e
Vitor Felde <feldevitor@gmail.com>
2026-07-31 20:23:59 -0300

    Add initial implementation of year handling program

---
+++ aula 01 introdução-nivelamento.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int year = 2021;
+int get_days_of_year (int leap_year){
+    //logica nervosa pra descobrir a qtd de dias
+
+    return 365;
+
+}
+
+int main () {
+    
+    int year = 2026;
+
+    printf ("Hello from %d!", year);
+    
+return 0;
+    
+}
