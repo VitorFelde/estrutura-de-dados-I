3b186f4cd75795aee35b254b9277fad928f9f4dd
Vitor Felde <feldevitor@gmail.com>
2026-07-31 21:17:10 -0300

    Create aula1pt2.c

---
+++ aula 01 /introdução e nivelamento/aula1pt2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void altera (int x) { //copia
+    x = 99;
+}
+
+void altera_mesmo (int *x) { //ponteiro (hack)
+    //acesso ao endereço de memória
+    /*se eu quiser mudar algo em outro escopo usamos o ponteiro*/
+    *x = 99;
+
+}
+
+int main () {
+
+    int num = 10;
+    printf ("%i\n", num); //10
+    altera (num); //passagem por valor (copia)
+    printf ("%i\n", num); //10
+
+    altera_mesmo (&num); //passagem por referencia
+    printf ("%i\n", num); //99
+
+return 0;
+    
+}
