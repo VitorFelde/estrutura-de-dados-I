08ed6c22cf6808ede9c627d83347536fa487f231
Vitor Felde <feldevitor@gmail.com>
2026-08-21 21:51:34 -0300

    Add files via upload

--- aula 03 - modularização/estoque_read.c
+++ aula 03 - modularização/estoque_read.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h> // malloc, free
 #include "produto.h"
 
 // Lê o arquivo e retorna um array dinâmico
 Produto* ler_produtos(const char *filename, int *quantidade_lida) {
-    FILE *arquivo = fopen(filename, "rb"); //read binary
+    FILE *arquivo = fopen(filename, "rb"); // read binary
     if (arquivo == NULL) {
         *quantidade_lida = 0;
         return NULL;
@@ -29,26 +30,37 @@ Produto* ler_produtos(const char *filename, int *quantidade_lida) {
         *quantidade_lida = 0;
         return NULL;
     }
-    printf ("1 - Memória alocada");
+    printf("\n1 - Memoria alocada");
 
     // Lê todos os produtos de uma vez para dentro do array
     fread(array_produtos, sizeof(Produto), *quantidade_lida, arquivo);
-    printf ("2 - Conteudo do arquivo salvo no array");
+    printf("\n2 - Conteudo do arquivo salvo no array");
     fclose(arquivo);
 
     return array_produtos;
 }
-
-void imprime_estoque (Produto *produtos) {
-    int tamanho=sizeof(produtos);
-    for (int i=0;i<tamanho;i++) {
-        printf ("%d %s", produtos[i].id, produtos[i].nome);
+void imprime_estoque(Produto *produtos, int tamanho) {
+    printf("\n--- ESTOQUE ---\n");
+    if (produtos==NULL) {
+        printf("estoque vazio");
+        return;
+    }
+    for(int i=0;i < tamanho; i++){
+        printf("\n%d %s %.2f %d", 
+            produtos[i].id, 
+            produtos[i].nome,
+            produtos[i].valor,
+            produtos[i].quantidade
+        );
     }
 }
-
 int main(){
     int quantidade_lida = 0;
     Produto *produtos = ler_produtos(ARQUIVO_ESTOQUE, &quantidade_lida);
-    imprime_estoque(produtos);
-
+    
+    imprime_estoque(produtos, quantidade_lida);
+    //  liberar a memoria alocada pelo malloc()
+    if (produtos != NULL) {
+        free(produtos);
+    }
 }
