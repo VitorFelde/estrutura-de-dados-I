a6a8f55899121699d1235ff99fe3f4f2700bdfd2
Vitor Felde <feldevitor@gmail.com>
2026-09-04 17:20:22 -0300

    Update aluno_update.c

--- atividade função + struct/CRUD fera demais/aluno_update.c
+++ atividade função + struct/CRUD fera demais/aluno_update.c
@@ -2,12 +2,12 @@
 #include <string.h>
 
 void atualizar_aluno(int id_alvo, int opcao) {
-    FILE *arquivo = fopen("alunos.bin", "r+b"); 
-    if (arquivo == NULL) return;
+    FILE *arquivo = fopen("alunos.bin", "r+b");//r+b para ler e modificar  
+    if (arquivo == NULL) return; //verificação de segurança
     
     Alunos a;
     while (fread(&a, sizeof(Alunos), 1, arquivo) == 1) {
-        if (a.id == id_alvo) {
+        if (a.id == id_alvo) { //pra procurar enquanto o id nao for igual
             if (opcao == 1) { 
                 printf("Nova Nota 1: "); scanf("%f", &a.p1);
                 printf("Nova Nota 2: "); scanf("%f", &a.p2);
@@ -21,12 +21,14 @@ void atualizar_aluno(int id_alvo, int opcao) {
             }
             
             a.media = (a.p1 + a.p2) / 2.0;
-            if (a.media >= 6.0 && a.falta <= 20) {
+            if (a.media >= 6 && a.falta <= 20) {
                 strcpy(a.situacao, "Aprovado");
             } else {
                 strcpy(a.situacao, "Reprovado");
             }
-            
+
+            // como o fread fez o cursor avançar para o próximo, voltamos o cursor
+            // um bloco para trás (tamanho do aluno) para sobrescrever o registro correto
             fseek(arquivo, -sizeof(Alunos), SEEK_CUR); // Volta o cursor
             fwrite(&a, sizeof(Alunos), 1, arquivo); // Sobrescreve
             fclose(arquivo);
@@ -35,5 +37,5 @@ void atualizar_aluno(int id_alvo, int opcao) {
         }
     }
     fclose(arquivo);
-    printf("Nao encontrado.\n");
+    printf("Nao encontrado\n");
 }
