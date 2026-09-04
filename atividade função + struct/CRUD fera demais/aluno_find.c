0294bddefbfe604533b0869304e2e4fae2a49f52
Vitor Felde <feldevitor@gmail.com>
2026-09-04 11:43:22 -0300

    Update aluno_find.c

--- atividade função + struct/CRUD fera demais/aluno_find.c
+++ atividade função + struct/CRUD fera demais/aluno_find.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-int buscar_aluno(int id_busca, char nome_busca[], Alunos *resultado) {
+int buscar_aluno(int idBusca, char nomeBusca[], Alunos *resultado) { 
     FILE *arquivo = fopen("alunos.bin", "rb");
-    if (arquivo == NULL) return 0;
+    if (arquivo == NULL) return 0; //verificação igual dos slides
     
-    Alunos a;
-    while (fread(&a, sizeof(Alunos), 1, arquivo) == 1) {
-        if (a.id == id_busca || (nome_busca != NULL && strcmp(a.nome, nome_busca) == 0)) {
+    Alunos a; //mais uma vez criando para melhor controle
+    while (fread(&a, sizeof(Alunos), 1, arquivo) == 1) { 
+        //sizeof ve o tamanho que os alunos ocupam na memoria em bytes
+        if (a.id == idBusca || (nomeBusca != NULL && strcmp(a.nome, nomeBusca) == 0)) { 
+            //fazendo comparaçõe de string para ver se o nome digitado for igual a algum que existe
             *resultado = a;
+            //como precisamos retornar se achou ou nao + dados dos alunos, precisamos usar ponteiro para que
+            //essa variavel em c consiga fazer isso fera demais
             fclose(arquivo);
             return 1;
         }
