e77d8039271153049d6277321341d7c1ff6cdd53
Vitor Felde <feldevitor@gmail.com>
2026-09-04 12:03:34 -0300

    Update aluno_init.c

--- atividade função + struct/CRUD fera demais/aluno_init.c
+++ atividade função + struct/CRUD fera demais/aluno_init.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <time.h>
+//incluindo biblioteca do horario 
 
-int gerar_id() { 
-    return (int)time(NULL); 
+int gerar_id() { //aqui esta a funcao que puxamos no create
+    return (int)time(NULL);  
+    //aqui pegamos o tempo, transformamos em int para poder gerar o id fera demais 
 }
 
 void gera_arquivo_alunos() {
     FILE *arquivo = fopen("alunos.bin", "rb");
-    if (arquivo != NULL) {
+    if (arquivo != NULL) {//verificação fera demais 
         fclose(arquivo);
         return; // Se abriu, o arquivo já existe. Não faz nada.
     }
@@ -15,6 +17,8 @@ void gera_arquivo_alunos() {
     arquivo = fopen("alunos.bin", "wb");
     if (arquivo == NULL) return;
 
+
+    //verificar isso
     Alunos a1 = {gerar_id(), "Joao", 111, 10, 7.0, 8.0, 7.5, "Aprovado"};
     Alunos a2 = {gerar_id() + 1, "Maria", 222, 25, 5.0, 4.0, 4.5, "Reprovado"};
 
