a467c20381b49faccff995ad20165836b56502e4
Vitor Felde <feldevitor@gmail.com>
2026-09-04 11:22:40 -0300

    added comments

--- atividade função + struct/CRUD fera demais/aluno.h
+++ atividade função + struct/CRUD fera demais/aluno.h
@@ -1,6 +1,7 @@
 #ifndef ALUNO_H
 #define ALUNO_H
-
+/*isso é uma verificação para segurança para que esse aluno.h não
+possa ser incluido 2 vezes sem querer em um dos outros arquivos*/
 typedef struct {
     int id;
     char nome[50];
@@ -10,4 +11,4 @@ typedef struct {
     char situacao[10];
 } Alunos;
 
-#endif
+#endif //fim da verificação fera demais 
