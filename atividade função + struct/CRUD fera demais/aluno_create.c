2e97b3132b5edb859868674fadd41cc47a9a4d58
Vitor Felde <feldevitor@gmail.com>
2026-09-04 11:36:22 -0300

    added comments

--- atividade função + struct/CRUD fera demais/aluno_create.c
+++ atividade função + struct/CRUD fera demais/aluno_create.c
@@ -2,13 +2,14 @@
 #include <string.h>
 
 void inserir_aluno() {
-    Alunos a;
-    a.id = gerar_id();
+    Alunos a; //criamos a variavel para controle mais facil dos dados
+    a.id = gerar_id(); 
+    //essa função gerar id é criada no init, e depois na main chamamos tudo e dai funciona fera demais
     
-    while (getchar() != '\n'); 
+    while (getchar() != '\n'); //limpeza de buffer lendaria
     printf("Nome do aluno: ");
     fgets(a.nome, sizeof(a.nome), stdin);    
-    a.nome[strcspn(a.nome, "\n")] = '\0'; 
+    a.nome[strcspn(a.nome, "\n")] = '\0'; //limpando o buffer debaixo agora
     
     printf("Matricula: ");
     scanf("%d", &a.matricula);
@@ -19,18 +20,21 @@ void inserir_aluno() {
     printf("Nota 2: ");
     scanf("%f", &a.p2);
     
-    a.media = (a.p1 + a.p2) / 2.0;
+    a.media = (a.p1 + a.p2) / 2;
     
     if (a.media >= 6.0 && a.falta <= 20) {
-        strcpy(a.situacao, "Aprovado"); 
+        strcpy(a.situacao, "Aprovado"); //passando o valor pra string com o strcpy
     } else {
         strcpy(a.situacao, "Reprovado");
     }
 
-    FILE *arquivo = fopen("alunos.bin", "ab");
+    FILE *arquivo = fopen("alunos.bin", "ab"); //abrindo um arquivo com esse nome
+    //usamos ab para que o programa nao exclua os antigos alunos do cadastro e coloque
+    //os novos na ultima posicao do nosso vetor de struct Alunos
     if (arquivo != NULL) {
         fwrite(&a, sizeof(Alunos), 1, arquivo);
         fclose(arquivo);
-        printf("\nAluno salvo! ID: %d\n", a.id);
+        printf("\nAluno salvo - ID: %d\n", a.id); 
+        //aqui printamos o id para que o usuario possa buscar pelo mesmo dps
     }
 }
