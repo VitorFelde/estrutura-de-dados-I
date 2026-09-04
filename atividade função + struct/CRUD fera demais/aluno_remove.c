e1eef84d9143af7cf7751b915dc3510213156aa5
Vitor Felde <feldevitor@gmail.com>
2026-09-04 17:16:09 -0300

    Update aluno_remove.c

--- atividade função + struct/CRUD fera demais/aluno_remove.c
+++ atividade função + struct/CRUD fera demais/aluno_remove.c
@@ -3,11 +3,15 @@
 void remover_aluno(int id_alvo) {
     FILE *original = fopen("alunos.bin", "rb");
     FILE *temp = fopen("temp.bin", "wb");
+    //aqui criamos um arquivo temporario apenas para servir de peneira e remover apenas o bloco que queremos
+    //se nao criarmos existe a chance do arquivo corromper
     
     Alunos a;
     int achou = 0;
     
     while (fread(&a, sizeof(Alunos), 1, original) == 1) {
+        //se o aluno nao for igual ao id que queremos excluir ele é salvo no arquivo temporario
+        //e dai no arquivo original fica livre apenas para o que iremos excluir
         if (a.id != id_alvo) {
             fwrite(&a, sizeof(Alunos), 1, temp);
         } else {
@@ -21,9 +25,9 @@ void remover_aluno(int id_alvo) {
     if (achou) {
         remove("alunos.bin"); 
         rename("temp.bin", "alunos.bin"); 
-        printf("Removido com sucesso!\n");
+        printf("aluno removido\n");
     } else {
         remove("temp.bin");
-        printf("Aluno nao encontrado.\n");
+        printf("aluno nao encontrado.\n");
     }
 }
